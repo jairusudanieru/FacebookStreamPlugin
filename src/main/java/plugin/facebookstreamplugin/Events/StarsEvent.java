c4225e170c9b26d0f61d30ee4cf729df4fd0943b
jairusu <jairusudanieru@gmail.com>
2023-05-11 00:04:34 +0800

    facebookstreamplugin

--- src/main/java/plugin/facebookstreamplugin/Events/StarsEvent.java
+++ src/main/java/plugin/facebookstreamplugin/Events/StarsEvent.java
@@ -1,57 +1,30 @@
 package plugin.facebookstreamplugin.Events;
 
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
-import org.bukkit.Sound;
-import org.bukkit.boss.BarColor;
-import org.bukkit.boss.BarStyle;
-import org.bukkit.boss.BossBar;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import plugin.facebookstreamplugin.Utilities.SendAlert;
 
 public class StarsEvent {
+
+    SendAlert sendAlert = new SendAlert();
 
     public void onStarsEvent(String name, String amount, JavaPlugin plugin) {
         //Configuration settings
         boolean messageType = plugin.getConfig().getBoolean("messageType");
         boolean logMessages = plugin.getConfig().getBoolean("logMessages");
-        boolean playSound = plugin.getConfig().getBoolean("playSound");
-
-        //Get the streamer name to which the alert will be sent
-        String streamerName = plugin.getConfig().getString("streamerName");
-        if (streamerName == null) streamerName = "player";
-        Player player =  Bukkit.getPlayer(streamerName);
 
         //Get the message that will be sent to the player
         String message = plugin.getConfig().getString("eventsMessages.stars");
         String consoleMessage = "[Facebook] " + name + " sent " + amount + " stars!";
         if (message == null || message.isEmpty()) return;
-        message = message
-                .replace("&","§")
-                .replace("%name%",name)
-                .replace("%amount%",amount);
+        message = message.replace("&","§").replace("%name%",name).replace("%amount%",amount);
 
         //Sending the message to the player
-        if (player == null || !player.isOnline()) { return; }
-        if (logMessages) { Bukkit.getLogger().info(consoleMessage); }
-        if (playSound) { player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 20f, 1f); }
+        if (logMessages) Bukkit.getLogger().info(consoleMessage);
         if (!messageType) {
-            BossBar bossBar = Bukkit.createBossBar(message, BarColor.WHITE, BarStyle.SEGMENTED_10);
-            bossBar.addPlayer(player);
-            bossBar.setProgress(1.0);
-            bossBar.setVisible(true);
-            Bukkit.getScheduler().runTaskTimer(plugin, () -> {
-                double progress = bossBar.getProgress();
-                if (progress > 0.3333) {
-                    bossBar.setProgress(progress - 0.3333);
-                } else {
-                    bossBar.setVisible(false);
-                    bossBar.removeAll();
-                }
-            }, 20L, 20L);
+            sendAlert.sendBossBar(plugin, message);
         } else {
-            player.sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(message));
+            sendAlert.sendActionBar(plugin, message);
         }
 
     }
---
+++ src/main/java/plugin/facebookstreamplugin/Utilities/SendAlert.java
@@ -0,0 +1,60 @@
+package plugin.facebookstreamplugin.Utilities;
+
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SendAlert {
+
+    //Using the bossBar to send the event message
+    public void sendBossBar(JavaPlugin plugin, String message) {
+        //Get the streamer name to which the alert will be sent
+        String streamerName = plugin.getConfig().getString("streamerName");
+        if (streamerName == null) streamerName = "player";
+        Player player =  Bukkit.getPlayerExact(streamerName);
+        if (player == null || !player.isOnline()) return;
+
+        //Check if the playSound option is enabled
+        boolean playSound = plugin.getConfig().getBoolean("playSound");
+        if (playSound) { player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 20f, 1f); }
+
+        //Send the bossBar to the player
+        BossBar bossBar = Bukkit.createBossBar(message, BarColor.WHITE, BarStyle.SEGMENTED_10);
+        bossBar.addPlayer(player);
+        bossBar.setProgress(1.0);
+        bossBar.setVisible(true);
+        Bukkit.getScheduler().runTaskTimer(plugin, () -> {
+            double progress = bossBar.getProgress();
+            if (progress > 0.3333) {
+                bossBar.setProgress(progress - 0.3333);
+            } else {
+                bossBar.setVisible(false);
+                bossBar.removeAll();
+            }
+        }, 20L, 20L);
+    }
+
+    //Using the actionBar to send the event message
+    public void sendActionBar(JavaPlugin plugin, String message) {
+        //Get the streamer name to which the alert will be sent
+        String streamerName = plugin.getConfig().getString("streamerName");
+        if (streamerName == null) streamerName = "player";
+        Player player =  Bukkit.getPlayerExact(streamerName);
+        if (player == null || !player.isOnline()) return;
+
+        //Check if the playSound option is enabled
+        boolean playSound = plugin.getConfig().getBoolean("playSound");
+        if (playSound) { player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 20f, 1f); }
+
+        //Send the actionBar to the player
+        player.sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(message));
+    }
+
+
+}
